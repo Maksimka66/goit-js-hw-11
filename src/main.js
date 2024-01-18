b9efe832c3d03be91cbaf361686d5d32afb14a11
Maksimka66 <maxfese@gmail.com>
2024-01-18 04:39:09 +0200

    the main part of the code

--- src/main.js
+++ src/main.js
@@ -11,29 +11,70 @@ const formToFill = document.querySelector('.form');
 const inputToFill = document.querySelector('input[name="delay"]');
 const galleryOfPictures = document.querySelector('.gallery');
 
-const params = new URLSearchParams({
-  key: '41859392-e5bc4a8d4ece805d6453ecbd7',
-  q: inputToFill.value,
-  image_photo: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-});
-
-// Запит на сервер
-const pictures = fetch('<https://pixabay.com/api/>', JSON.stringify(params))
-  .then(response => {
-    if (!response.ok) {
+// Слухач форми
+formToFill.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchQuery = inputToFill.value.trim();
+  if (searchQuery === '') {
+    iziToast.warning({
+      title: 'All fields must be filled!',
+      position: 'topRight',
+    });
+  }
+  fetchImages(searchQuery)
+    .then(arr => {
+      const markup = arr
+        .map(
+          ({
+            webformatURL,
+            largeImageURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+          }) =>
+            `<li class="gallery-item">
+      <a  href="${largeImageURL}">
+        <img 
+          class="gallery-image"
+          src="${webformatURL}"
+          alt="${tags}"
+        />
+        <p>Likes: ${likes}</p>
+        <p>Views: ${views}</p>
+        <p>Comments: ${comments}</p>
+        <p>Downloads: ${downloads}</p>
+      </a>
+    </li>`
+        )
+        .join('');
+      galleryOfPictures.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error =>
       iziToast.error({
         title:
           'Sorry, there are no images matching your search query. Please try again!',
         position: 'topRight',
-      });
+      })
+    );
+});
+
+// Набір параметрів
+function fetchImages(value) {
+  const params = new URLSearchParams({
+    key: '41859392-e5bc4a8d4ece805d6453ecbd7',
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  // Запит на сервер
+  return fetch(`https://pixabay.com/api/?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error('Error fetching images');
     }
     return response.json();
-  })
-  .then();
-
-// Слухач форми
-formToFill.addEventListener('submit', event => {
-  event.preventDefault();
-});
+  });
+}
