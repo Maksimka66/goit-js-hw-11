7ba307aa12eca446c322c7a31fc4dc44cb0af71b
Maksimka66 <maxfese@gmail.com>
2024-01-20 21:27:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-// Імпорт simplelightbox'
+// Імпорт simplelightbox
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
